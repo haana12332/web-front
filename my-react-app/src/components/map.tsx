0e84893a907222c80700c5521b254cb651303df0
Arai <araihotaka5@gmail.com>
2024-10-18 23:41:33 +0900

    sliding http

--- my-react-app/src/components/map.tsx
+++ my-react-app/src/components/map.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import DisplayMap from "@/components/map/displayMap.jsx";
 import { Gps } from "@/type/type";
-// import { GpsContext } from "./context/gpsdata";
+import { GpsContext } from "./context/gpsdata";
 
 const apikey: string = import.meta.env.VITE_API_KEY;
 console.log("apikey", apikey);
 
 export function Map() {
-  // const gpsContext = useContext(GpsContext); // gpsContext全体を取得
+  const gpsContext = useContext(GpsContext); // gpsContext全体を取得
   const [gps, setGps] = useState<Gps>({
     lat: 35.6814568602531, // 初期位置
     lng: 139.76799772026422, // 初期位置
@@ -23,10 +23,9 @@ export function Map() {
         const newGps = { lat: latitude, lng: longitude };
         setGps(newGps);
         // gpsContextが存在する場合にのみ更新を行う
-        // if (gpsContext && gpsContext.setGpsDate) {
-        //   console.log(newGps);
-        //   gpsContext.setGpsDate(newGps);
-        // }
+        if (gpsContext && gpsContext.setGpsDate) {
+          gpsContext.setGpsDate(newGps);
+        }
       },
       (error) => {
         console.error("Error retrieving location:", error);
